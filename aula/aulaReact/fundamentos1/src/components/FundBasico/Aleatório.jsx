1e3f0692046964eebbd93b3160972bae4e84edb9
Jefferson Luiz Araujo da Silva <79008632+jffLuiz@users.noreply.github.com>
2021-09-23 21:05:46 -0300

    .

--- aula/aulaReact/fundamentos1/src/components/FundBasico/Aleatório.jsx
+++ aula/aulaReact/fundamentos1/src/components/FundBasico/Aleatório.jsx
@@ -3,16 +3,12 @@ import React from "react"
 export default function numAleatorio(props){
     let min=props.min 
     let max=props.max
-    // let {min, max} = props //resumida
+    
 
     let random = parseInt(Math.random()*(max-min)+min)
 
     
     return (
-        <React.Fragment>
-            <h2>valor minimo: {props.min}</h2>
-            <h2>valor maximo: {props.max}</h2>
-            <h2>Numero Aleatório: {random}</h2>
-        </React.Fragment>
+            <h2>{random}</h2>
     )
 }
